11c0921ee1796b52021c0c7d85233a1514f32058
Noah Wolfe <darthnoah.wolfe@gmail.com>
2017-01-10 15:20:26 -0500

    Added page render for index.ejs

--- controllers/index.js
+++ controllers/index.js
@@ -6,6 +6,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   req.io.sockets.emit('echo', 'THIS IS THE SERVER');
+  res.render('index', { title: 'Arogya' });
 });
 
 module.exports = router;
