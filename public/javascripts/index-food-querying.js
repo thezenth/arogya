431fab3edc224649ef4a2a9ff28b0be33b58eb80
Noah Wolfe <darthnoah.wolfe@gmail.com>
2017-01-13 15:28:17 -0500

    Reorganization of javascripts
    - Makes debugging a lot easier! As well, makes index.ejs much smaller

---
+++ public/javascripts/index-food-querying.js
@@ -0,0 +1,52 @@
+/* variables specific to this page */
+var newMeal = {
+  foods: [],
+};
+
+/* These are functions specific to this page */
+function emitFoodQuery() {
+  console.log('hello!');
+  emitValue(
+    '_relevant_foods_request',
+    { foodQuery: document.getElementById('foodQuery').value }
+  );
+}
+
+function beginNewMeal() {
+  // create meal object/clear out any existing ones
+  newMeal = { foods: [] };
+  //newMeal.foods = [];
+
+  // delete create meal button
+  document.getElementById("newMealButton").outerHTML = "";
+  delete document.getElementById("newMealButton");
+
+  // instantiate other html stuff
+  var queryInput = document.createElement("input");
+  queryInput.id = "foodQuery";
+  queryInput.name = "foodQuery";
+  queryInput.type = "text";
+  queryInput.onchange = function(){ emitFoodQuery(); };
+  document.body.appendChild(queryInput);
+
+  var finishMealButton = document.createElement("button");
+  finishMealButton.innerHTML = "Done?";
+  finishMealButton.onclick = function(){ finishMeal(); };
+  document.body.appendChild(finishMealButton);
+}
+
+function finishMeal() {
+  console.dir(newMeal);
+  socket.emit('_finished_meal', { meal: newMeal });
+}
+
+function addFoodToMeal(f) {
+  console.dir(f);
+  // push to the new meal being constructed
+  newMeal.foods.push(f);
+  // delete any autocomplete divs
+  if(document.getElementById("autocomplete-div")) {
+    document.getElementById("autocomplete-div").outerHTML = "";
+    delete document.getElementById("autocomplete-div");
+  }
+}
---
+++ public/javascripts/index-socket-listeners.js
@@ -0,0 +1,62 @@
+/* listeners specific to this page */
+socket.on('_relevant_foods_returned', function(data) {
+  var foods = data.items;
+
+  // delete any previous autocomplete divs
+  if(document.getElementById("autocomplete-div")) {
+    document.getElementById("autocomplete-div").outerHTML = "";
+    delete document.getElementById("autocomplete-div");
+  }
+
+  // load autocomplete div template (i.e., the big box for the results)
+  var div = document.createElement("div");
+  div.id = "autocomplete-div";
+  div.className = "autocomplete-div-class";
+
+  for (var i = 0; i<foods.length; i++) {
+    var button = document.createElement("button");
+    button.className = "autocomplete-div-row-class";
+    button.innerHTML = foods[i].name;
+    var setFood = foods[i];
+    console.dir(setFood);
+    button.onclick = function() {
+      addFoodToMeal(setFood);
+    }
+    div.appendChild(button);
+  }
+
+  document.body.appendChild(div);
+});
+
+socket.on('_relevant_foods_not_found', function(data) {
+  // delete any previous autocomplete divs
+  if(document.getElementById("autocomplete-div")) {
+    document.getElementById("autocomplete-div").outerHTML = "";
+    delete document.getElementById("autocomplete-div");
+  }
+
+  // log the error client side as well
+  console.error(data.errorMsg);
+});
+
+socket.on('_saved_meal_to_db', function(data) {
+  // delete everything
+  document.body.innerHTML = "";
+
+  // quick message
+  var newP = document.createElement("p");
+  newP.innerHTML = "Saved!";
+  document.body.appendChild(newP);
+
+  // wait a specified amount of time to delete this message, and then add back the addMeal button
+  var time = 3 * 1000;
+  setTimeout(function() {
+    document.body.innerHTML = "";
+    // add back the newMealButton
+    var newMealButton = document.createElement("button");
+    newMealButton.id = "newMealButton";
+    newMealButton.onClick = function(){ beginNewMeal(); };
+    newMealButton.innerHTML = "Create a new meal";
+    document.body.appendChild(newMealButton);
+  }, time)
+});
