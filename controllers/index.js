e39eadf7e58948d2cc19e13f056dbef6efb076c6
Noah Wolfe <darthnoah.wolfe@gmail.com>
2017-01-13 10:06:11 -0500

    Included /warnings route in controller master

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+router.use('/warnings', require('./warnings'));
+
 /* GET home page. */
 router.get('/', function(req, res) {
   req.io.sockets.emit('echo', 'THIS IS THE SERVER');
