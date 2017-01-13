ec9f55c92a7a1fc18e1d3494d6efe319660201fb
Noah Wolfe <darthnoah.wolfe@gmail.com>
2017-01-13 17:22:13 -0500

    Fixed partials function

--- public/javascripts/dynamic-partials.js
+++ public/javascripts/dynamic-partials.js
@@ -1,9 +1,5 @@
-function callPartial() {
-
-  var reader = new FileReader();
-  var partialStr = reader.readAsText('/partials/test.ejs');
-
-  $("body").append(
-    ejs.render(partialStr, { msg: "Hello! I'm a partial!" })
+function renderPartial(parentId, partial, data) {
+  $(parentId).append(
+    ejs.render(partial, data)
   );
 }
