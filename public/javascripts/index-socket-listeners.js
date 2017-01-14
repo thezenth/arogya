2da36bbb9934967abd8c7ef5bc5c1503328abebd
Noah Wolfe <darthnoah.wolfe@gmail.com>
2017-01-14 11:03:10 -0500

    Fixed buttons in ejs for message if food already exists

--- public/javascripts/index-socket-listeners.js
+++ public/javascripts/index-socket-listeners.js
@@ -100,8 +100,8 @@ socket.on('_return_food_exist_check', function(data) {
       `
       <div id="food-exist-msg">
         <p>You've already told me about a food with the same name; do you still want to save it?</p>
-        </button onclick="addConstructedFoodToMeal(${JSON.stringify(data.food)});">Yes</button>
-        </button onclick="deleteExistsMsg();">No</button>
+        <button onclick="addConstructedFoodToMeal(${JSON.stringify(data.food)});">Yes</button>
+        <button onclick="deleteExistsMsg();">No</button>
       </div>
       `
     );
